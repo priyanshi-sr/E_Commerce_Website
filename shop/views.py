9fb190e0b818167ee5b0919601d11f97f1776314
Leetcode-Problems <priyanshisrivastava906@gmail.com>
2024-03-24 17:02:19 +0530

    fixed index.html

--- shop/views.py
+++ shop/views.py
@@ -119,22 +119,22 @@ def checkout(request):
 
 @csrf_exempt
 def handelerequest(request):
-    return HttpResponse('done')
-    pass
-    # form = request.POST
-    # response_dict = {}
-    # for i in form.keys():
-    #     response_dict[i] = form[i]
-    #     if i == 'CHECKSUMHASH':
-    #         checksum = form[i]
-    #
-    # verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
-    # if verify:
-    #     if response_dict['RESPCODE'] == '01':
-    #         print('order successful')
-    #     else:
-    #         print('order was not successful because' + response_dict['RESPMSG'])
-    # return render(request, 'shop/paymentstatus.html', {'response': response_dict})
+    # return HttpResponse('done')
+    # pass
+    form = request.POST
+    response_dict = {}
+    for i in form.keys():
+        response_dict[i] = form[i]
+        if i == 'CHECKSUMHASH':
+            checksum = form[i]
+
+    verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+    if verify:
+        if response_dict['RESPCODE'] == '01':
+            print('order successful')
+        else:
+            print('order was not successful because' + response_dict['RESPMSG'])
+    return render(request, 'shop/paymentstatus.html', {'response': response_dict})
 
 
 def blog(request):
