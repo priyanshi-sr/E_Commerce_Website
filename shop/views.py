c657202d86ea3215d302960930fb91530001c063
Leetcode-Problems <priyanshisrivastava906@gmail.com>
2024-03-28 15:32:42 +0530

    fixed index.html

--- shop/views.py
+++ shop/views.py
@@ -69,7 +69,42 @@ def tracker(request):
 
 
 def search(request):
-    return HttpResponse("We are at search")
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query) < 4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
+
+
+def searchMatch(query, item):
+    # Ensure query is not empty or None
+    if not query:
+        return False
+
+    # Ensure item is not None
+    if item is None:
+        return False
+
+    # Ensure item attributes are not None
+    if item.desc is None or item.product_name is None or item.category is None:
+        return False
+
+    # Perform search operation
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
 
 
 def productView(request, myid):
